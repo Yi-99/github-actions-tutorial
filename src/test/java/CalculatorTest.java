eb359a99bab70c7c9d35b2e87837bdda80c6330f
Yi-99 <ylim.8299@gmail.com>
2025-01-10 22:52:07 -0700

    feat: WIP

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-public class CalculatorTest { 
+public class CalculatorTest {  
 
     @Test
     public void addTest(){
