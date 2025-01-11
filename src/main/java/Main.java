fb9afcc72857daf0b1d525bf95ffebe551a1680e
Yi-99 <ylim.8299@gmail.com>
2025-01-11 12:10:31 -0700

    feat: calculator object used in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Testing Dockerfile");
+
+        Calculator c = new Calculator();
+
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+
+        switch (args[0]) {
+            case "add":
+                System.out.println(c.add(a, b));
+                break;
+            case "subtract":
+                System.out.println(c.subtract(a, b));
+                break;
+            case "multiply":
+                System.out.println(c.multiply(a, b));
+                break;
+            case "binary":
+                System.out.println(c.intToBinaryNumber(a));
+                break;
+        }
     }
 }
